614f16372866120d217b7ccb67b9311e49ddd488
Dodo <kasper199914@gmail.com>
2019-05-28 21:15:49 +0200

    featuregate the 8bit impls

--- src/bit_8_impls/mod.rs
+++ src/bit_8_impls/mod.rs
@@ -1,33 +1,72 @@
 use crate::*;
 use core::{arch::x86_64::*, ops::*};
 
+#[cfg(target_feature = "sse2")]
 mod i8_16_impl;
+#[cfg(target_feature = "avx2")]
 mod i8_32_impl;
+#[cfg(target_feature = "sse2")]
 mod u8_16_impl;
+#[cfg(target_feature = "avx2")]
 mod u8_32_impl;
 
+// ------------------------------------
+// ---------- impl add ----------------
+//-------------------------------------
+#[cfg(target_feature = "sse2")]
 binops!(u8, 16, Add, add, _mm_add_epi8);
+#[cfg(target_feature = "sse2")]
 binops!(i8, 16, Add, add, _mm_add_epi8);
+#[cfg(target_feature = "avx2")]
 binops!(u8, 32, Add, add, _mm256_add_epi8);
+#[cfg(target_feature = "avx2")]
 binops!(i8, 32, Add, add, _mm256_add_epi8);
 
+// ------------------------------------
+// ---------- impl bitand -------------
+//-------------------------------------
+#[cfg(target_feature = "sse2")]
 binops!(u8, 16, BitAnd, bitand, _mm_and_si128);
+#[cfg(target_feature = "sse2")]
 binops!(i8, 16, BitAnd, bitand, _mm_and_si128);
+#[cfg(target_feature = "avx2")]
 binops!(u8, 32, BitAnd, bitand, _mm256_and_si256);
+#[cfg(target_feature = "avx2")]
 binops!(i8, 32, BitAnd, bitand, _mm256_and_si256);
 
+// ------------------------------------
+// ---------- impl bitor --------------
+//-------------------------------------
+#[cfg(target_feature = "sse2")]
 binops!(u8, 16, BitOr, bitor, _mm_or_si128);
+#[cfg(target_feature = "sse2")]
 binops!(i8, 16, BitOr, bitor, _mm_or_si128);
+#[cfg(target_feature = "avx2")]
 binops!(u8, 32, BitOr, bitor, _mm256_or_si256);
+#[cfg(target_feature = "avx2")]
 binops!(i8, 32, BitOr, bitor, _mm256_or_si256);
 
+// ------------------------------------
+// ---------- impl bitxor -------------
+//-------------------------------------
+#[cfg(target_feature = "sse2")]
 binops!(u8, 16, BitXor, bitxor, _mm_xor_si128);
+#[cfg(target_feature = "sse2")]
 binops!(i8, 16, BitXor, bitxor, _mm_xor_si128);
+#[cfg(target_feature = "avx2")]
 binops!(u8, 32, BitXor, bitxor, _mm256_xor_si256);
+#[cfg(target_feature = "avx2")]
 binops!(i8, 32, BitXor, bitxor, _mm256_xor_si256);
 
+// ------------------------------------
+// ---------- impl sub ----------------
+//-------------------------------------
+#[cfg(target_feature = "sse2")]
 binops!(u8, 16, Sub, sub, _mm_sub_epi8);
+#[cfg(target_feature = "sse2")]
 binops!(i8, 16, Sub, sub, _mm_sub_epi8);
+#[cfg(target_feature = "avx2")]
 binops!(u8, 32, Sub, sub, _mm256_sub_epi8);
+#[cfg(target_feature = "avx2")]
 binops!(i8, 32, Sub, sub, _mm256_sub_epi8);
 
--- src/float_impls/mod.rs
+++ src/float_impls/mod.rs
@@ -1,11 +1,18 @@
 use crate::*;
 use core::{arch::x86_64::*, ops::*};
 
+#[cfg(target_feature = "sse")]
 mod f32_4_impl;
+#[cfg(target_feature = "avx")]
 mod f32_8_impl;
+#[cfg(target_feature = "sse2")]
 mod f64_2_impl;
+#[cfg(target_feature = "avx")]
 mod f64_4_impl;
 
+// ------------------------------------
+// ---------- impl add ----------------
+//-------------------------------------
 #[cfg(target_feature = "sse")]
 binops!(f32, 4, Add, add, _mm_add_ps);
 #[cfg(target_feature = "avx")]
@@ -15,6 +22,9 @@ binops!(f64, 2, Add, add, _mm_add_pd);
 #[cfg(target_feature = "avx")]
 binops!(f64, 4, Add, add, _mm256_add_pd);
 
+// ------------------------------------
+// ---------- impl bitand -------------
+//-------------------------------------
 #[cfg(target_feature = "sse")]
 binops!(f32, 4, BitAnd, bitand, _mm_and_ps);
 #[cfg(target_feature = "avx")]
@@ -24,6 +34,9 @@ binops!(f64, 2, BitAnd, bitand, _mm_and_pd);
 #[cfg(target_feature = "avx")]
 binops!(f64, 4, BitAnd, bitand, _mm256_and_pd);
 
+// ------------------------------------
+// ---------- impl bitor --------------
+//-------------------------------------
 #[cfg(target_feature = "sse")]
 binops!(f32, 4, BitOr, bitor, _mm_or_ps);
 #[cfg(target_feature = "avx")]
@@ -33,6 +46,9 @@ binops!(f64, 2, BitOr, bitor, _mm_or_pd);
 #[cfg(target_feature = "avx")]
 binops!(f64, 4, BitOr, bitor, _mm256_or_pd);
 
+// ------------------------------------
+// ---------- impl bitxor -------------
+//-------------------------------------
 #[cfg(target_feature = "sse")]
 binops!(f32, 4, BitXor, bitxor, _mm_xor_ps);
 #[cfg(target_feature = "avx")]
@@ -42,6 +58,9 @@ binops!(f64, 2, BitXor, bitxor, _mm_xor_pd);
 #[cfg(target_feature = "avx")]
 binops!(f64, 4, BitXor, bitxor, _mm256_xor_pd);
 
+// ------------------------------------
+// ---------- impl mul ----------------
+//-------------------------------------
 #[cfg(target_feature = "sse")]
 binops!(f32, 4, Mul, mul, _mm_mul_ps);
 #[cfg(target_feature = "avx")]
@@ -58,6 +77,9 @@ binops!(u16, 16, Shr, shr, _mm256_sll_epi16);
 binops!(i16, 16, Shr, shr, _mm256_sll_epi16);
 */
 
+// ------------------------------------
+// ---------- impl sub ----------------
+//-------------------------------------
 #[cfg(target_feature = "sse")]
 binops!(f32, 4, Sub, sub, _mm_sub_ps);
 #[cfg(target_feature = "avx")]

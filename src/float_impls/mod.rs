0d7212b13c77165293a0a0e58d536af619d486a8
Dodo <kasper199914@gmail.com>
2019-05-28 20:59:26 +0200

    conditional compilation for the float types

--- src/float_impls/mod.rs
+++ src/float_impls/mod.rs
@@ -6,29 +6,49 @@ mod f32_8_impl;
 mod f64_2_impl;
 mod f64_4_impl;
 
+#[cfg(target_feature = "sse")]
 binops!(f32, 4, Add, add, _mm_add_ps);
+#[cfg(target_feature = "avx")]
 binops!(f32, 8, Add, add, _mm256_add_ps);
+#[cfg(target_feature = "sse2")]
 binops!(f64, 2, Add, add, _mm_add_pd);
+#[cfg(target_feature = "avx")]
 binops!(f64, 4, Add, add, _mm256_add_pd);
 
+#[cfg(target_feature = "sse")]
 binops!(f32, 4, BitAnd, bitand, _mm_and_ps);
+#[cfg(target_feature = "avx")]
 binops!(f32, 8, BitAnd, bitand, _mm256_and_ps);
+#[cfg(target_feature = "sse2")]
 binops!(f64, 2, BitAnd, bitand, _mm_and_pd);
+#[cfg(target_feature = "avx")]
 binops!(f64, 4, BitAnd, bitand, _mm256_and_pd);
 
+#[cfg(target_feature = "sse")]
 binops!(f32, 4, BitOr, bitor, _mm_or_ps);
+#[cfg(target_feature = "avx")]
 binops!(f32, 8, BitOr, bitor, _mm256_or_ps);
+#[cfg(target_feature = "sse2")]
 binops!(f64, 2, BitOr, bitor, _mm_or_pd);
+#[cfg(target_feature = "avx")]
 binops!(f64, 4, BitOr, bitor, _mm256_or_pd);
 
+#[cfg(target_feature = "sse")]
 binops!(f32, 4, BitXor, bitxor, _mm_xor_ps);
+#[cfg(target_feature = "avx")]
 binops!(f32, 8, BitXor, bitxor, _mm256_xor_ps);
+#[cfg(target_feature = "sse2")]
 binops!(f64, 2, BitXor, bitxor, _mm_xor_pd);
+#[cfg(target_feature = "avx")]
 binops!(f64, 4, BitXor, bitxor, _mm256_xor_pd);
 
+#[cfg(target_feature = "sse")]
 binops!(f32, 4, Mul, mul, _mm_mul_ps);
+#[cfg(target_feature = "avx")]
 binops!(f32, 8, Mul, mul, _mm256_mul_ps);
+#[cfg(target_feature = "sse2")]
 binops!(f64, 2, Mul, mul, _mm_mul_pd);
+#[cfg(target_feature = "avx")]
 binops!(f64, 4, Mul, mul, _mm256_mul_pd);
 
 /*
@@ -38,7 +58,11 @@ binops!(u16, 16, Shr, shr, _mm256_sll_epi16);
 binops!(i16, 16, Shr, shr, _mm256_sll_epi16);
 */
 
+#[cfg(target_feature = "sse")]
 binops!(f32, 4, Sub, sub, _mm_sub_ps);
+#[cfg(target_feature = "avx")]
 binops!(f32, 8, Sub, sub, _mm256_sub_ps);
+#[cfg(target_feature = "sse2")]
 binops!(f64, 2, Sub, sub, _mm_sub_pd);
+#[cfg(target_feature = "avx")]
 binops!(f64, 4, Sub, sub, _mm256_sub_pd);
